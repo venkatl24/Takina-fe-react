af9e4d63aa1b078b86cc93451a51def154aee939
vkrishnanb <vkrishnanb@yahoo.com>
2024-10-19 01:11:06 +0530

    included node modules build working

---
+++ src/scripts/MyAbout.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class MyAbout extends React.Component {
+  render() {
+    return (
+         <div className="alert alert-info">
+                <h1>About</h1>
+            </div>
+    );
+  }
+}
+
+export default MyAbout;
